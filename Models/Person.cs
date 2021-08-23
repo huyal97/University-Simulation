e5168e1f3fec8aa44d2239d1ab7a3b3ef2266a51
Hüseyin Yalçınkaya <h.yalcinkaya97@gmail.com>
2021-08-23 10:35:56 +0300

    Add person,student and teacher model

---
+++ Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University_Simulation.Models
+{
+    class Person
+    {
+        public string _Name { get; set; }
+        public string _Surname { get; set; }
+        public string _Role { get; set; }
+        public string _Department { get; set; }
+
+        public Person()
+        {
+            
+        }
+        public void Fullname()
+        {
+            Console.WriteLine($"{_Name} {_Surname}");
+        }
+    }
+}
