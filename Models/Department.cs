d216cea260c395c052e8be7bbca7ee14ec08315f
Hüseyin Yalçınkaya <h.yalcinkaya97@gmail.com>
2021-08-23 10:30:14 +0300

    Add department and university model

---
+++ Models/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University_Simulation.Models
+{
+    class Department
+    {
+        public string _Name { get; set; }
+        public int _MidtermScoreRatio { get; set; }
+        public int _FinalScoreRatio { get; set; }
+        public int _MinimumPassingScore { get; set; }
+        public string _UniversityName { get; set; }
+        public Department(string line)
+        {
+            
+        }
+        public void FullName()
+        {
+            Console.WriteLine($"{_UniversityName} {_Name} Department")
+        }
+    }
+    
+}
---
+++ Models/University.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University_Simulation.Models
+{
+    public class University
+    {
+        public string _Name { get; set; }
+        public int _DataOfEstablishment { get; set; }
+        
+
+        public University()
+        {
+            
+        }
+        void Students()
+        {
+
+        }
+    }
+}
